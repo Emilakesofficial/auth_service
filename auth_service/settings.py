13579245a7344e916b7fb414201a8bc3550b7f1f
Emilakesofficial <157388465+Emilakesofficial@users.noreply.github.com>
2025-08-31 21:01:14 +0100

    made changes to urls.py

--- auth_service/settings.py
+++ auth_service/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", default=False, cast=bool)
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["https://auth-service-znxh.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definition
